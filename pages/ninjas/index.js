4b1d3937f6c2824817f718d5fa05c81e28794e88
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-05-02 18:45:29 +0300

    fetch data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,25 @@
-const index = () => {
+import styles from '../../styles/Ninjas.module.css'
+
+export let getStaticProps = async () => {
+  let res = await fetch('http://jsonplaceholder.typicode.com/users')
+  let data = await res.json()
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const index = ( { ninjas } ) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      { ninjas.map(item => (
+        <div key={ item.id }>
+          <a className={ styles.single }>
+            <h3>{ item.name }</h3>
+          </a>
+        </div>
+          
+      )) }
     </div>
   );
 }
